6e83d0b1d5fa750144d59d4a3724eb6c5da09229
Darío Rivera <fermius.us@gmail.com>
2020-11-24 18:09:42 -0500

    feat: library scaffolding

---
+++ src/Calculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Pleets\LaravelPayPal;
+
+class Calculator
+{
+    public function sum($a, $b)
+    {
+        return $a + $b;
+    }
+}
---
+++ tests/CalculatorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Pleets\LaravelPayPal;
+
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itCanTestSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
