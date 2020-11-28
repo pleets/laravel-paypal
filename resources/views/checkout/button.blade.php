6c113e10b97be4fe5fdf552740dee866e33ab131
Darío Rivera <fermius.us@gmail.com>
2020-11-27 23:09:19 -0500

    feat: add smart payment buttons (checkout) integation

---
+++ resources/views/checkout/button.blade.php
@@ -0,0 +1,4 @@
+<!-- smart payment button -->
+@if(config('paypal.checkout.activated'))
+    <div id="paypal-smart-payment-button"></div>
+@endif
---
+++ src/LaravelPaypalProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Pleets\LaravelPayPal;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelPaypalProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->loadConfig();
+        $this->loadViews();
+    }
+
+    private function loadConfig(): void
+    {
+        $this->publishes(
+            [
+                __DIR__ . '/../config/paypal' => config_path('paypal'),
+            ],
+            'laravel-paypal'
+        );
+    }
+
+    private function loadViews(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'laravel-paypal');
+
+        $this->publishes(
+            [
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/laravel-paypal'),
+                __DIR__ . '/../public/js' => public_path('js/paypal'),
+            ],
+            'laravel-paypal'
+        );
+    }
+}
