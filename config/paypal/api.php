a87936cc896208f4db2d36343b1dc905517e2933
Darío Rivera <fermius.us@gmail.com>
2020-12-26 12:16:16 -0500

    feat: add paypal environments

--- config/paypal/api.php
+++ config/paypal/api.php
@@ -5,4 +5,9 @@ return [
         'client_id' => env('PAYPAL_CLIENT_ID'),
         'secret' => env('PAYAL_SECRET'),
     ],
+    'endpoints' => [
+        'sandbox' => 'https://api.sandbox.paypal.com',
+        'live' => 'https://api.paypal.com'
+    ],
+    'environment' => env('PAYPAL_ENVIRONMENT', 'sandbox')
 ];
