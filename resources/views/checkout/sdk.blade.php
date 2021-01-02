9d5e365bf47d0cb6bb584e543e8703923e3f527f
Darío Rivera <fermius.us@gmail.com>
2021-01-02 09:54:31 -0500

    fix: client-id in template

--- resources/views/checkout/sdk.blade.php
+++ resources/views/checkout/sdk.blade.php
@@ -1,4 +1,4 @@
 <!-- PayPay checkout sdk -->
 @if(\Pleets\LaravelPayPal\Helpers\Environment::isCheckoutActivated())
-    <script src="https://www.paypal.com/sdk/js?client-id={{ config('paypal.api.credentials.client_id') }}"></script>
+    <script src="https://www.paypal.com/sdk/js?client-id={{ \Pleets\LaravelPayPal\Helpers\Environment::getClientId() }}"></script>
 @endif
