4a1041f7e918eb31e120f2da402ebeccea93d460
Darío Rivera <fermius.us@gmail.com>
2021-01-07 20:04:55 -0500

    refactor: get activation from helper

--- resources/views/subscriptions/sdk.blade.php
+++ resources/views/subscriptions/sdk.blade.php
@@ -1,4 +1,4 @@
-<!-- PayPay checkout sdk -->
+<!-- PayPay subscriptions sdk -->
 @if(\Pleets\LaravelPayPal\Helpers\Environment::areSubscriptionsActivated())
     <script src="https://www.paypal.com/sdk/js?client-id={{ \Pleets\LaravelPayPal\Helpers\Environment::getClientId() }}&vault=true"></script>
 @endif
